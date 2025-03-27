731cff562f733eb0ad97313ccc1bddb59bad7e26
George Gorzhiyev <gegorz@hotmail.com>
2025-03-27 15:41:29 -0600

    Added rebase token interface to use in vault

---
+++ src/interfaces/IRebaseToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+interface IRebaseToken {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
+    function balanceOf(address _account) external view returns (uint256);
+}
